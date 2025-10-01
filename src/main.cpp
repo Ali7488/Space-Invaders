f00cc608e5f1d35b366a131dc43159d8865f8e05
Mohannad Emad <MOHANNAD.EALI@gmail.com>
2025-10-01 00:46:27 -0600

    created windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,46 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description)
+{
+    std::cerr << "Error: " << description << std::endl;
+}
+
 int main()
 {
+    glfwSetErrorCallback(error_callback); //just incase of errors
+    //Initialize GLFW, check it initialized and return error if failed
     if(!glfwInit()){
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1; 
     }
+    //create an 640x480 window called Space indvaders saved into window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Space Invaders", nullptr, nullptr);
+    if (!window) //checking window was created correctly
+    {
+        std::cerr << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); //tells OpenGL to render into window
 
+
+    if(glewInit() != GLEW_OK) //initialize GLEW to use latest OpenGL version and check it loaded
+    {
+        std::cerr << "Could not load GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glClearColor(1.0, 0.0, 0.0, 1.0); //sets background to opaque red every time a frame refreshes
+    while(!glfwWindowShouldClose(window)) //while user has NOT pressed X to close the window
+    {
+        glClear(GL_COLOR_BUFFER_BIT); //clear the color buffer
+        glfwSwapBuffers(window); //display the frame that was just drawn (swap back buffer and front buffer)
+        glfwPollEvents(); //check for user inputs
+    }
+
+    //deleting windows and clearing resources
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
